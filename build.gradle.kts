6f23da6a4d8dcefb3fda90219b7f72dce2a10595
Md. Nahian Lamim <127649683+wgnahianlamim@users.noreply.github.com>
2026-04-29 18:25:42 +0600

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.10.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+}
